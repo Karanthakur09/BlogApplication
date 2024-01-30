a422a962b977331561a416927d64a6dc902efb7e
Karan <kstkaranthakur03@gmail.com>
2024-01-30 23:10:11 +0530

    building independent components

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,14 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
 export{
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Button,
+    Input
 }
