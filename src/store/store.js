7b6690fa8712f006cbd6596d08dfbc1b06e26038
Karan <kstkaranthakur03@gmail.com>
2024-02-01 01:09:15 +0530

    adding more components

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,8 @@ import { configureStore } from "@reduxjs/toolkit";
 const store = configureStore(
     {
         reducer: {//you need to list all the reducers 
+            
+            
 
         }
     }
