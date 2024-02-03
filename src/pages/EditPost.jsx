9aa30a55823d19f60cdba153de88444dd5a732c9
Karan <kstkaranthakur03@gmail.com>
2024-02-04 02:41:11 +0530

    adding more pages form home and posts

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -4,6 +4,30 @@ import appwriteService from "../appwrite/config";
 import { useNavigate, useParams } from 'react-router-dom';
 
 function EditPost() {
+    const [post, setPosts] = useState(null)
+    const { slug } = useParams();
+    const navigate = useNavigate();
+    //we will get the value from slug
+    useEffect(() => {
+        if (slug) {
+            appwriteService.getPost(slug).then((post) => {
+                if (post) {
+                    setPosts(post)
+                }
+            })
+        } else {
+            navigate('/')
+        }
+    }, [slug, navigate])
+
+    return (
+        <div className='py-8'>
+            <Container>
+                <PostForm post={post} />
+            </Container>
+        </div>
+
+    )
 }
 
 export default EditPost;
